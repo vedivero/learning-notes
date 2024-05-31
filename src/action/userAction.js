9e7487e397962933a09b546dd3596a3c972c3c00
vedivero <vedivero@github.com>
2024-06-01 01:30:20 +0900

    add production back-end URL

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -2,15 +2,34 @@ import api from "../utils/api";
 import * as types from "../constants/user.constants";
 import { commonUiActions } from "./commonUiAction";
 import * as commonTypes from "../constants/commonUI.constants";
-const loginWithToken = () => async (dispatch) => {};
-const loginWithEmail = (payload) => async (dispatch) => {};
-const logout = () => async (dispatch) => {};
+import { useNavigate } from "react-router";
+const loginWithToken = () => async (dispatch) => { };
+const loginWithEmail = (payload) => async (dispatch) => { };
+const logout = () => async (dispatch) => { };
 
-const loginWithGoogle = (token) => async (dispatch) => {};
+const loginWithGoogle = (token) => async (dispatch) => { };
 
 const registerUser =
   ({ email, name, password }, navigate) =>
-  async (dispatch) => {};
+    async (dispatch) => {
+      try {
+        dispatch({ type: types.REGISTER_USER_REQUEST });
+        //api 호출
+        const response = await api.post("/user", { email, name, password });
+        if (response.status != 200) throw new Error("respons.error");
+        dispatch({ type: types.REGISTER_USER_SUCCESS });
+        //성공 메세지
+        dispatch(
+          commonUiActions.showToastMessage("회원 가입이 완료되었습니다.", "success")
+        );
+        //redirection (함수형 컴포넌트안에서만 사용 가능)
+        navigate("/login")
+      } catch (error) {
+        dispatch({ type: types.REGISTER_USER_FAIL, payload: error.error })
+      }
+    };
+
+
 export const userActions = {
   loginWithToken,
   loginWithEmail,
--- src/page/RegisterPage.js
+++ src/page/RegisterPage.js
@@ -6,6 +6,8 @@ import { userActions } from "../action/userAction";
 import "../style/register.style.css";
 const RegisterPage = () => {
   const dispatch = useDispatch();
+
+  //회원가입 입력 폼 객체화, 사용자가 입력한 값 읽어오기
   const [formData, setFormData] = useState({
     email: "",
     name: "",
@@ -13,6 +15,7 @@ const RegisterPage = () => {
     confirmPassword: "",
     policy: false,
   });
+
   const navigate = useNavigate();
   const [passwordError, setPasswordError] = useState("");
   const [policyError, setPolicyError] = useState(false);
@@ -20,15 +23,43 @@ const RegisterPage = () => {
 
   const register = (event) => {
     event.preventDefault();
+
+    //form의 데이터 전체 읽어오기
+    const { name, email, password, confirmPassword, policy } = formData;
     // 비번 중복확인 일치하는지 확인
-    // 이용약관에 체크했는지 확인
+    if (password !== confirmPassword) {
+      return setPasswordError("입력하신 비밀번호가 서로 일치하지 않습니다.");
+    }
+    // 이용약관에 체크하지 않은 경우
+    if (!policy) {
+      return setPolicyError(true)
+    }
+
+    //에러메세지 초기화
+    setPasswordError("");
+    setPolicyError(false);
+
     // FormData에 있는 값을 가지고 백엔드로 넘겨주기
+    dispatch(userActions.registerUser({ name, email, password }, navigate));
+
     //성공후 로그인 페이지로 넘어가기
+
   };
 
   const handleChange = (event) => {
-    event.preventDefault();
+
     // 값을 읽어서 FormData에 넣어주기
+    //데이터를 입력할 때 마다 받아오는 값
+    const { id, value, checked } = event.target;
+    console.log(id, checked)
+
+    if (id === "policy") {
+      setFormData({ ...formData, [id]: checked })
+    } else {
+      //전체 formData에서 id값의 value를 넣는다
+      setFormData({ ...formData, [id]: value });
+    }
+
   };
 
   return (
@@ -47,7 +78,7 @@ const RegisterPage = () => {
             type="email"
             id="email"
             placeholder="Enter email"
-            onChange={handleChange}
+            onChange={handleChange} //form에 입력할 때 마다 호출
             required
           />
         </Form.Group>
