f6b8ee3fd376ed3ee92804a15c5cd438abb3e220
vedivero <vedivero@github.com>
2024-06-01 20:57:31 +0900

    modify baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_LOCAL_BACKEND,
+  baseURL: process.env.REACT_APP_BACKEND_PROXY,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
