7091f43d3387ae6a7aa7f54e1b2f4ff00c4c93df
vedivero <vedivero@github.com>
2024-06-01 21:18:51 +0900

    modify baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,22 @@
 import axios from "axios";
+// 상황따라 주소 다름
+const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
+const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
+const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+
+console.log("PROD_BACKEND : ", PROD_BACKEND);
+console.log("BACKEND_PROXY : ", BACKEND_PROXY);
+
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_PROXY,
+  baseURL: BACKEND_PROXY,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
   },
 });
 
-console.log("baseURL : ", process.env.REACT_APP_BACKEND_PROXY);
+console.log("baseURL : ", api.baseURL);
 /**
  * console.log all requests and responses
  */
