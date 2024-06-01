da0d91051716bfe10f5eaa14bcc29805c81a0150
vedivero <vedivero@github.com>
2024-06-01 15:05:36 +0900

    modify baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
 const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-  baseURL: BACKEND_PROXY,
+  baseURL: BACKEND_PROXY + `/api`,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
