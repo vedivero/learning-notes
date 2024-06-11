5ac0d849006970e49b4a7499ea18d2c0f2726f82
vedivero <vedivero@github.com>
2024-06-12 00:05:02 +0900

    login with google

--- routes/authApi.js
+++ routes/authApi.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
 
-
+//로그인
 router.post('/login', authController.loginWithEmail);
+//구글 로그인
+router.post('/google', authController.loginWithGoogle);
 
 
 module.exports = router;
