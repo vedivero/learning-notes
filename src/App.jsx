6b89c2ff2b5ffa965d917baf462293a435eca6b8
vedivero <vedivero@github.com>
2024-07-19 17:14:23 +0900

    setting todo list State

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import Editor from './components/Editor';
 import Header from './components/Header';
 import List from './components/List';
 
+//테스트를 위한 임시 테이터. 각각의 todoItem을 객체형태로 저장
+//다시 생성하지 않도록 컴포넌트 외부에 선언
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '운동하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '청소하기',
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className='App'>
       <Header />
