86b957f506d02493f6b9be96748658e49ca5ab63
vedivero <vedivero@github.com>
2024-07-19 20:36:19 +0900

    landering to do data screen

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,15 @@
 import './List.css';
 import TodoItem from './TodoItem';
-const List = () => {
+const List = ({ todos }) => {
   return (
     <div className='List'>
       <h4>Todo List🪴</h4>
       <input placeholder='검색어를 입력해 주세요.' />
       <div className='todos_wrapper'>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => {
+          //리스트 형태로 랜더링된 컴포넌트 요소들을 구분할 때 `key`를 사용
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
