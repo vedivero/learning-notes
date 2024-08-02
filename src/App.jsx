cc900c632d7c2ee8b74d63f53e86028942d81019
vedivero <vedivero@github.com>
2024-08-02 20:12:12 +0900

    폰트, 이미지, 레이아웃 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Diary from './pages/Diary';
 import Notfound from './pages/Notfound';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
+import { getEmotionImage } from './util/get-emotion-image';
+
 //  1. "/" : 모든 일기를 조회하는 Home page
 //  2. "/new" : 새로운 일기를 작성하는 새로운 페이지
 //  3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
@@ -19,6 +21,13 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={'/'}>Home</Link>
         <Link to={'/new'}>New</Link>
