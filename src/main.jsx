16b21e30020dcb666241af0d41406d10f162de81
vedivero <vedivero@github.com>
2024-08-04 17:28:42 +0900

    공통 컴포넌트 구현(Header, Button)

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-//BrowserRouter = 현재 주소를 저장하고 감지
-//모든 컴포넌트가 현재 브라우저의 주소를 사용
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
