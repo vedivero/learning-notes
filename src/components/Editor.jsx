4141486b88a9609e050529890448f35335155a76
vedivero <vedivero@github.com>
2024-08-12 19:17:17 +0900

    일기 수정하기 기능 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,144 +1,153 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from './Button';
 import './Editor.css';
 import EmotionItem from './EmotionItem';
 import { useNavigate } from 'react-router-dom';
 
 const emotionList = [
-  {
-    emotionId: 1,
-    emotionName: '아주 좋음',
-  },
-  {
-    emotionId: 2,
-    emotionName: '좋음',
-  },
-  {
-    emotionId: 3,
-    emotionName: '그럭저럭',
-  },
-  {
-    emotionId: 4,
-    emotionName: '안 좋음',
-  },
-  {
-    emotionId: 5,
-    emotionName: '아주 안 좋음',
-  },
+   {
+      emotionId: 1,
+      emotionName: '아주 좋음',
+   },
+   {
+      emotionId: 2,
+      emotionName: '좋음',
+   },
+   {
+      emotionId: 3,
+      emotionName: '그럭저럭',
+   },
+   {
+      emotionId: 4,
+      emotionName: '안 좋음',
+   },
+   {
+      emotionId: 5,
+      emotionName: '아주 안 좋음',
+   },
 ];
 
 //Date객체를 문자열로 변환하는 함수
 const getStringedDate = (targetDate) => {
-  //"YYYY-MM-DD"
-  let year = targetDate.getFullYear();
-  let month = targetDate.getMonth() + 1;
-  let date = targetDate.getDate();
+   //"YYYY-MM-DD"
+   let year = targetDate.getFullYear();
+   let month = targetDate.getMonth() + 1;
+   let date = targetDate.getDate();
 
-  //10이하의 숫자 처리 (1 -> 01)
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  if (date < 10) {
-    date = `0${date}`;
-  }
+   //10이하의 숫자 처리 (1 -> 01)
+   if (month < 10) {
+      month = `0${month}`;
+   }
+   if (date < 10) {
+      date = `0${date}`;
+   }
 
-  return `${year}-${month}-${date}`;
+   return `${year}-${month}-${date}`;
 };
 
-const Editor = ({ onSubmit }) => {
-  //사용자가 입력한 값을 저장하는 State
-  //날짜, 감정, 내용 저장
-  //여러 값을 저장하기 위해 객체로 만들어서 사용
-  const [input, setInput] = useState({
-    createdDate: new Date(),
-    emotionId: 3,
-    content: '',
-  });
+const Editor = ({ initData, onSubmit }) => {
+   //사용자가 입력한 값을 저장하는 State
+   //날짜, 감정, 내용 저장
+   //여러 값을 저장하기 위해 객체로 만들어서 사용
+   const [input, setInput] = useState({
+      createdDate: new Date(),
+      emotionId: 3,
+      content: '',
+   });
 
-  const nav = useNavigate();
+   const nav = useNavigate();
 
-  //사용자가 날짜 설정하는 경우
-  const onChangeInput = (e) => {
-    let name = e.target.name; //값이 들어온 요소
-    let value = e.target.value; //입력된 값
+   useEffect(() => {
+      if (initData) {
+         setInput({
+            ...initData,
+            createdDate: new Date(
+               Number(initData.createdDate),
+            ),
+         });
+      }
+   }, [initData]);
 
-    //문자열로 전달되는 날짜 값을 날짜 객체로 변환
-    if (name === 'createdDate') {
-      value = new Date(value);
-    }
+   //사용자가 날짜 설정하는 경우
+   const onChangeInput = (e) => {
+      let name = e.target.name; //값이 들어온 요소
+      let value = e.target.value; //입력된 값
 
-    //`input` State 값 변경
-    setInput({
-      ...input, //스프레드 연산자 사용해서 기존 State 값 유지
-      [name]: value, //입력이 발생한 요소 설정 : 수정할 값
-    });
-  };
+      //문자열로 전달되는 날짜 값을 날짜 객체로 변환
+      if (name === 'createdDate') {
+         value = new Date(value);
+      }
 
-  //작성 완료 이벤트
-  const onClickSubmitButton = () => {
-    onSubmit(input);
-  };
+      //`input` State 값 변경
+      setInput({
+         ...input, //스프레드 연산자 사용해서 기존 State 값 유지
+         [name]: value, //입력이 발생한 요소 설정 : 수정할 값
+      });
+   };
 
-  return (
-    <div className='Editor'>
-      <section className='date_section'>
-        <h4>오늘의 날짜</h4>
-        <input
-          name='createdDate'
-          onChange={onChangeInput}
-          value={getStringedDate(
-            input.createdDate,
-          )}
-          type='date'
-        />
-      </section>
-      <section className='emotion_section'>
-        <h4>오늘의 감정</h4>
-        <div className='emotion_list_wrapper'>
-          {emotionList.map((item) => (
-            <EmotionItem
-              onClick={() =>
-                onChangeInput({
-                  target: {
-                    name: 'emotionId',
-                    value: item.emotionId,
-                  },
-                })
-              }
-              key={item.emotionId}
-              {...item}
-              isSelected={
-                item.emotionId === input.emotionId
-              }
+   //작성 완료 이벤트
+   const onClickSubmitButton = () => {
+      onSubmit(input);
+   };
+
+   return (
+      <div className='Editor'>
+         <section className='date_section'>
+            <h4>오늘의 날짜</h4>
+            <input
+               name='createdDate'
+               onChange={onChangeInput}
+               value={getStringedDate(input.createdDate)}
+               type='date'
             />
-          ))}
-        </div>
-      </section>
-      <section className='content_section'>
-        <h4>오늘의 일기</h4>
-        <textarea
-          onChange={onChangeInput}
-          name='content'
-          value={input.content}
-          placeholder='오늘 하루는 어떠셨나요?'
-        />
-      </section>
-      <section className='button_section'>
-        <Button
-          onClick={() => {
-            nav(-1);
-          }}
-          text={'취소'}
-          type='NEGATIVE'
-        />
-        <Button
-          onClick={onClickSubmitButton}
-          text={'작성'}
-          type='POSITIVE'
-        />
-      </section>
-    </div>
-  );
+         </section>
+         <section className='emotion_section'>
+            <h4>오늘의 감정</h4>
+            <div className='emotion_list_wrapper'>
+               {emotionList.map((item) => (
+                  <EmotionItem
+                     onClick={() =>
+                        onChangeInput({
+                           target: {
+                              name: 'emotionId',
+                              value: item.emotionId,
+                           },
+                        })
+                     }
+                     key={item.emotionId}
+                     {...item}
+                     isSelected={
+                        item.emotionId === input.emotionId
+                     }
+                  />
+               ))}
+            </div>
+         </section>
+         <section className='content_section'>
+            <h4>오늘의 일기</h4>
+            <textarea
+               onChange={onChangeInput}
+               name='content'
+               value={input.content}
+               placeholder='오늘 하루는 어떠셨나요?'
+            />
+         </section>
+         <section className='button_section'>
+            <Button
+               onClick={() => {
+                  nav(-1);
+               }}
+               text={'취소'}
+               type='NEGATIVE'
+            />
+            <Button
+               onClick={onClickSubmitButton}
+               text={'작성 완료'}
+               type='POSITIVE'
+            />
+         </section>
+      </div>
+   );
 };
 
 export default Editor;
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,82 @@
-import { useParams } from 'react-router-dom'; //URL 파라미터를 꺼내올 수 있는 커스텀 훅
+import { useParams, useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+import { useContext, useEffect, useState } from 'react';
+import {
+   DiaryDispatchContext,
+   DiaryStateContext,
+} from '../App';
 
 const Edit = () => {
-  const param = useParams();
-  return <div>{param.id}번 일기입니다.</div>;
+   const params = useParams(); //URL파라미터를 추출하는 Hook
+   const nav = useNavigate();
+   const { onDelete, onUpdate } = useContext(
+      DiaryDispatchContext,
+   ); //onDelet함수를 useContext를 통해 DiaryDispatchContext로부터 호출
+
+   const data = useContext(DiaryStateContext); //useContext를 통해 DiaryStateContext로 부터 현재 일기 데이터 불러오기
+   const [curDiaryItem, setCurDiaryItem] = useState();
+
+   useEffect(() => {
+      // ID에 해당하는 일기 데이터 필터
+      const currentDiaryItem = data.find(
+         (item) => String(item.id) === String(params.id),
+      );
+      //데이터가 존재하지 안는 경우
+      if (!currentDiaryItem) {
+         window.alert('존재하지 않는 일기입니다.');
+         nav('/', { replace: true });
+      }
+
+      setCurDiaryItem(currentDiaryItem);
+   }, [params.id, data]); //params.id, data값이 변경될 때 실행되도록 설정
+
+   //일기 삭제
+   const onClickDelete = () => {
+      if (window.confirm('일기를 삭제할까요?')) {
+         // 일기 삭제 로직
+         onDelete(params.id);
+         nav('/', { replace: true }); //삭제 후 뒤로가기 방지처리
+      }
+   };
+
+   const onSubmit = (input) => {
+      if (window.confirm('일기를 수정할까요?')) {
+         onUpdate(
+            params.id,
+            input.createdDate.getTime(),
+            input.emotionId,
+            input.content,
+         );
+         nav('/', { replace: true });
+      }
+   };
+
+   return (
+      <div>
+         <Header
+            title={'일기 수정하기'}
+            leftChild={
+               <Button
+                  onClick={() => nav(-1)}
+                  text={'< 뒤로 가기'}
+               />
+            }
+            rightChild={
+               <Button
+                  onClick={onClickDelete}
+                  text={'삭제하기'}
+                  type={'NEGATIVE'}
+               />
+            }
+         />
+         <Editor
+            initData={curDiaryItem}
+            onSubmit={onSubmit}
+         />
+      </div>
+   );
 };
 
 export default Edit;
