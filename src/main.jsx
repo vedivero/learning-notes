2ec4deb2f096502211e8480f7aecf0097abd4d50
vedivero <vedivero@github.com>
2024-09-21 11:59:00 +0900

    setting redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store/index.js';
 
 createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
-      <App />
-   </BrowserRouter>,
+   <Provider store={store}>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </Provider>,
 );
---
+++ src/store/chatRoomSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+   currentChatRoom: {
+      craeteBy: {
+         image: '',
+         name: '',
+      },
+      description: '',
+      id: '',
+      name: '',
+   },
+   isPrivateChatRoom: false,
+   userPosts: null,
+};
+export const chatRoomSlice = createSlice({
+   name: 'chatRoom',
+   initialState,
+   reducers: {},
+});
+
+export default chatRoomSlice.reduce;
