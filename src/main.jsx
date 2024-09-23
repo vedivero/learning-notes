23d0ec06d6a57b54fbb01adacb51786639ceaa1a
vedivero <vedivero@github.com>
2024-09-23 10:59:31 +0900

    회원 가입 페이지 UI

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/index.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
    <Provider store={store}>
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
-   return <div>RegisterPage</div>;
+   return (
+      <div className='auth-wrapper'>
+         <div style={{ textAlign: 'center' }}>
+            <h3>Register</h3>
+         </div>
+         <form>
+            <label>Email</label>
+            <input name='email' type='email' id='email' />
+            <label htmlFor='name'>Name</label>
+            <input name='name' type='name' id='name' />
+            <label>Password</label>
+            <input name='password' type='password' id='password' />
+            <input type='submit' disabled />
+            <Link
+               style={{ color: 'gray', TextDecoration: 'none' }}
+               to={'/login'}
+            >
+               이미 아이디가 있다면...
+            </Link>
+         </form>
+      </div>
+   );
 };
 
 export default RegisterPage;
