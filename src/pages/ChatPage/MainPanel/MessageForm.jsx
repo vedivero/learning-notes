9e6cd2fbd0956e68b9d20ed6a1dcb7b3fa51ed76
vedivero <vedivero@github.com>
2024-09-26 00:17:34 +0900

    메세지 작성 내용 DB 저장 테스트

--- src/pages/ChatPage/MainPanel/MessageForm.jsx
+++ src/pages/ChatPage/MainPanel/MessageForm.jsx
@@ -1,8 +1,127 @@
 //메세지 작성 폼 컴포넌트
-import React from 'react';
+import {
+   child,
+   push,
+   set,
+   ref as dbRef,
+   serverTimestamp,
+} from 'firebase/database';
+import React, { useState } from 'react';
+import { db } from '../../../firebase';
+import { useSelector } from 'react-redux';
 
 const MessageForm = () => {
-   return <div>MessageForm</div>;
+   const [content, setContent] = useState(''); //메세지 내용
+   const [loading, setLoading] = useState(false); //전송
+   const [errors, setErrors] = useState([]); //에러
+
+   const messagesRef = dbRef(db, 'messages'); //참조하는 경로
+
+   const { currentChatRoom } = useSelector((state) => state.chatRoom);
+   const { currentUser } = useSelector((state) => state.user); //로그인 된 유저의 데이터
+   const { isPriavateChatRoom } = useSelector((state) => state.chatRoom); //Direct Message
+
+   const createMessage = (fileUrl = null) => {
+      const message = {
+         timestamp: serverTimestamp(),
+         user: {
+            id: currentUser.uid,
+            name: currentUser.displayName,
+            image: currentUser.photoURL,
+         },
+      };
+
+      if (fileUrl !== null) {
+         message['image'] = fileUrl;
+      } else {
+         message['content'] = content;
+      }
+
+      return message;
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      //메세지 입력 없이 전송한 경우
+      if (!content) {
+         setErrors((prev) => prev.concat('메세지 내용을 입력해 주세요.'));
+         return; //에러 발생 시, 이 위치에서 진행 종료
+      }
+      setLoading(true);
+
+      //farebase에 message를 저장하는 부분
+      try {
+         console.log('currentChatRoom.id : ', currentChatRoom.id);
+         console.log('messagesRef : ', messagesRef);
+         await set(
+            push(child(messagesRef, currentChatRoom.id)),
+            createMessage(),
+         );
+         setLoading(false);
+         setContent('');
+         setErrors([]);
+      } catch (error) {
+         console.log(error);
+         setErrors((prev) => prev.concat(error.message));
+
+         setLoading(false);
+         setTimeout(() => {
+            setErrors([]);
+         }, 5000);
+      }
+   };
+   return (
+      <div>
+         <form onSubmit={handleSubmit}>
+            <textarea
+               style={{
+                  width: '100%',
+                  height: 90,
+                  border: '0.2rem solid rgb(235,236,236',
+                  borderRadius: 4,
+               }}
+               value={content}
+               onChange={(e) => setContent(e.target.value)}
+            />
+            <div>
+               {errors.map((errorMsg, i) => (
+                  <p key={i} style={{ color: 'red' }}>
+                     {errorMsg}
+                  </p>
+               ))}
+            </div>
+            <div style={{ display: 'flex', gap: 16 }}>
+               <div style={{ flexGrow: 1 }}>
+                  <button
+                     type='submit'
+                     className='message-form-button'
+                     style={{ width: '100%', fontSize: 20, fontWeight: 'bold' }}
+                     disabled={loading}
+                  >
+                     보내기
+                  </button>
+               </div>
+               <div style={{ flexGrow: 1 }}>
+                  <button
+                     type='submit'
+                     className='message-form-button'
+                     style={{ width: '100%', fontSize: 20, fontWeight: 'bold' }}
+                     disabled={loading}
+                  >
+                     이미지
+                  </button>
+               </div>
+            </div>
+         </form>
+
+         <input
+            type='file'
+            accept='image/jpeg, image/png'
+            style={{ display: 'none' }}
+         />
+      </div>
+   );
 };
 
 export default MessageForm;
--- src/pages/ChatPage/MainPanel/MessageHeader.jsx
+++ src/pages/ChatPage/MainPanel/MessageHeader.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Image from 'react-bootstrap/Image';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { useSelector } from 'react-redux';
 
 const MessageHeader = () => {
-   return <div>MessageHeader</div>;
+   const { currentChatRoom } = useSelector((state) => state.chatRoom);
+
+   return (
+      <div
+         style={{
+            width: '100%',
+            height: '190px',
+            border: '.2rem solid #ececec',
+            borderRadius: '4px',
+            padding: '1rem',
+            marginBottom: '1rem',
+         }}
+      >
+         <Row>
+            <Col></Col>
+
+            <Col>
+               <InputGroup className='mb-3'>
+                  <InputGroup.Text id='basic-addon1'>
+                     <AiOutlineSearch />
+                  </InputGroup.Text>
+                  <FormControl
+                     onChange
+                     placeholder='Search Messages'
+                     aria-label='Search'
+                     aria-describedby='basic-addon1'
+                  />
+               </InputGroup>
+            </Col>
+         </Row>
+         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            {currentChatRoom?.createdBy && (
+               <>
+                  <Image
+                     src={currentChatRoom.createdBy.image}
+                     style={{ width: '30px', height: '30px', marginRight: 7 }}
+                     roundedCircle
+                  />{' '}
+                  <p>{currentChatRoom.createdBy.name}</p>
+               </>
+            )}
+         </div>
+
+         <Row>
+            <Col></Col>
+            <Col></Col>
+         </Row>
+      </div>
+   );
 };
 
 export default MessageHeader;
