b7b986f908bbff56de09d76f443f9024c5f4ae34
vedivero <vedivero@github.com>
2024-10-03 16:55:21 +0900

    회원가입 시, 프로필 및 아이디가 안 보이는 문제 해결

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import {
-   createUserWithEmailAndPassword,
-   getAuth,
-   updateProfile,
-} from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
 import { ref, set } from 'firebase/database';
 import app, { db } from '../../firebase';
 import md5 from 'md5';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../store/userSlice';
 
 const RegisterPage = () => {
    const auth = getAuth(app);
    //회원가입 제출 시, 다시 버튼을 누르지 못 하게끔
    const [loading, setLoading] = useState(false);
    const [errorFromSubmit, setErrorFromSubmit] = useState('');
+   const dispatch = useDispatch();
    const {
       register,
       formState: { errors },
@@ -24,19 +23,21 @@ const RegisterPage = () => {
    const onSubmit = async (data) => {
       try {
          setLoading(true);
-         const createdUser = await createUserWithEmailAndPassword(
-            auth,
-            data.email,
-            data.password,
-         );
+         const createdUser = await createUserWithEmailAndPassword(auth, data.email, data.password);
          console.log(createdUser);
 
          await updateProfile(auth.currentUser, {
             displayName: data.name,
-            photoURL: `http://gravatar.com/avatar/${md5(
-               createdUser.user.email,
-            )}?d=identicon`,
+            photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`,
          });
+
+         const userData = {
+            uid: createdUser.user.uid,
+            displayName: createdUser.user.displayName,
+            photoURL: createdUser.user.photoURL,
+         };
+
+         dispatch(setUser(userData));
 
          set(ref(db, `users/${createdUser.user.uid}`), {
             name: createdUser.user.displayName,
@@ -74,12 +75,8 @@ const RegisterPage = () => {
                id='name'
                {...register('name', { required: true, maxLength: 10 })}
             />
-            {errors.name && errors.name.type === 'required' && (
-               <p>이름을 입력해 주세요.</p>
-            )}
-            {errors.name && errors.name.type === 'maxLength' && (
-               <p>최대 입력 길이를 초과했습니다.</p>
-            )}
+            {errors.name && errors.name.type === 'required' && <p>이름을 입력해 주세요.</p>}
+            {errors.name && errors.name.type === 'maxLength' && <p>최대 입력 길이를 초과했습니다.</p>}
             <label>Password</label>
             <input
                name='password'
@@ -87,12 +84,8 @@ const RegisterPage = () => {
                id='password'
                {...register('password', { required: true, minLength: 6 })}
             />
-            {errors.password && errors.password.type === 'required' && (
-               <p>비밀번호를 입력해 주세요.</p>
-            )}
-            {errors.password && errors.password.type === 'maxLength' && (
-               <p>최소 6자리 이상 입력해 주세요.</p>
-            )}
+            {errors.password && errors.password.type === 'required' && <p>비밀번호를 입력해 주세요.</p>}
+            {errors.password && errors.password.type === 'maxLength' && <p>최소 6자리 이상 입력해 주세요.</p>}
 
             {errorFromSubmit && <p>{errorFromSubmit}</p>}
 
