ab9bed2e30eb2f17418bfc6c8d955d878d476046
vedivero <vedivero@github.com>
2024-10-15 17:54:59 +0900

    기본 폴더 구조 및 파일 생성

---
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,8 @@
+//로그인 하지 않은 유저가, 로그인이 필요한 페이지에 접근했을 경우를 제어
+import React from 'react';
+
+const ProtectedRoutes = () => {
+   return <div>ProtectedRoutes</div>;
+};
+
+export default ProtectedRoutes;
---
+++ frontend/src/pages/RegisterPage/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RegisterPAge = () => {
+   return <div>RegisterPAge</div>;
+};
+
+export default RegisterPAge;
