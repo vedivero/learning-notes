49a77917ac7fb7193f2cac6c6c88a2c2099738e3
vedivero <vedivero@github.com>
2024-10-15 17:54:59 +0900

    기본 폴더 구조 및 파일 생성

---
+++ frontend/src/components/NotAuthRoutes.jsx
@@ -0,0 +1,8 @@
+//로그인 된 유저가 '회원가입', '로그인' 페이지로 접근하지 못하도록 -> return main page
+import React from 'react';
+
+const NotAuthRoutes = () => {
+   return <div>NotAuthRoutes</div>;
+};
+
+export default NotAuthRoutes;
