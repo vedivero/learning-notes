25244547bd16f73f92329f991136b7df69f843ca
vedivero <vedivero@github.com>
2024-10-18 00:03:56 +0900

    CSS 전체 구조 및 react Icons적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet, Route, Routes } from 'react-router-dom';
+import './App.css';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Navbar from './layout/NavBar';
+import Footer from './layout/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+function Layout() {
+   return (
+      <div className='flex flex-col justify-between h-screen'>
+         <Navbar />
+         <main className='w-10/12 max-w-4xl mx-auto mb-auto'>
+            <Outlet />
+         </main>
+         <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   );
 }
 
-export default App
+function App() {
+   return (
+      <Routes>
+         <Route path='/' element={<Layout />}>
+            <Route index element={<LandingPage />} />
+            <Route path='login' element={<LoginPage />} />
+            <Route path='register' element={<RegisterPage />} />
+         </Route>
+      </Routes>
+   );
+}
+
+export default App;
