865616d75f8c94d3bbf5e440323d7076082403fd
vedivero <vedivero@github.com>
2024-10-18 03:03:24 +0900

    static middleware 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
    res.send('Hello World!');
 });
+
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.listen(port, () => {
    console.log(`Server ON, Port Number : ${port}`);
