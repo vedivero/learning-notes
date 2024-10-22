72c75e269667613f2c04bf232bd9cec0b95d9ca1
vedivero <vedivero@github.com>
2024-10-23 03:19:41 +0900

    사용자로부터 회원 정보 받아서 몽구스에 저장

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,15 +19,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-   throw new Error('Error');
-});
-
-app.post('/', (req, res) => {
-   console.log(req.body);
-   res.json(req.body);
-});
+app.use('/users', require('./routes/users'));
 
 app.use(express.static(path.join(__dirname, '../uploads')));
 
