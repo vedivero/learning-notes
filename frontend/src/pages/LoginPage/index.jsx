8e6c0abdb0643c918ec8a296f57a13028f144a0f
vedivero <vedivero@github.com>
2024-10-24 11:26:09 +0900

    로그인 페이지 UI 및 로그인 요청 로직

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -1,7 +1,75 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../store/thunkFunctions';
 
 const LoginPage = () => {
-   return <div>LoginPage</div>;
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      reset,
+   } = useForm({ mode: 'onChange' });
+
+   const dispatch = useDispatch();
+
+   const onSubmit = ({ email, password, name }) => {
+      const body = {
+         email,
+         password,
+      };
+
+      dispatch(loginUser(body));
+
+      reset();
+   };
+
+   return (
+      <section className='flex flex-col justify-center mt-20 max-w-[400px] m-auto'>
+         <div className='p-6 bg-white rounded-md shadow-md'>
+            <h1 className='text-3xl font-semibold text-center'>회원가입</h1>
+            <form className='mt-6' onSubmit={handleSubmit(onSubmit)}>
+               <div className='mb-2'>
+                  <label htmlFor='email' className='text-sm font-semibold text-gray-800'>
+                     Email
+                  </label>
+                  <input
+                     type='email'
+                     id='email'
+                     className='w-full px-4 py-2 mt-2 bg-white border rounded-md'
+                  />
+               </div>
+
+               <div className='mb-2'>
+                  <label htmlFor='password' className='text-sm font-semibold text-gray-800'>
+                     Password
+                  </label>
+                  <input
+                     type='password'
+                     id='password'
+                     className='w-full px-4 py-2 mt-2 bg-white border rounded-md'
+                  />
+               </div>
+
+               <div className='mt-6'>
+                  <button
+                     type='submit'
+                     className='w-full px-4 py-2 text-white duration-200 bg-black rounded-md hover:bg-gray-700'
+                  >
+                     로그인
+                  </button>
+               </div>
+
+               <p className='mt-8 text-xs font-light text-center text-gray-700'>
+                  아이디가 없다면?{' '}
+                  <a href='/register' className='font-medium hover:underline'>
+                     회원가입
+                  </a>
+               </p>
+            </form>
+         </div>
+      </section>
+   );
 };
 
 export default LoginPage;
