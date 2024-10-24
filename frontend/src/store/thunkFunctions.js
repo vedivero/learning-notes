3ed9add890a3ed8bb017d3615856ba4f818956ef
vedivero <vedivero@github.com>
2024-10-24 11:26:09 +0900

    로그인 페이지 UI 및 로그인 요청 로직

--- frontend/src/store/thunkFunctions.js
+++ frontend/src/store/thunkFunctions.js
@@ -10,3 +10,13 @@ export const registerUser = createAsyncThunk('user/registerUser', async (body, t
       return thunkAPI.rejectWithValue(error.response.data || error.message);
    }
 });
+
+export const loginUser = createAsyncThunk('user/registerUser', async (userData, thunkAPI) => {
+   try {
+      const response = await axiosInstance.post(`/users/login`, userData);
+      return response.data;
+   } catch (error) {
+      console.log('registerUser error : ', error);
+      return thunkAPI.rejectWithValue(error.response.data || error.message);
+   }
+});
