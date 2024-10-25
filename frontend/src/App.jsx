fff01b7a51ae064299811d1cc3550d2888e6454f
vedivero <vedivero@github.com>
2024-10-25 16:41:21 +0900

    로그인 하지 않은 접근자 처리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
@@ -7,11 +7,17 @@ import Navbar from './layout/NavBar';
 import Footer from './layout/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { authUser } from './store/thunkFunctions';
+import ProtectedPage from './pages/ProtectedPage';
+import ProtectedRoutes from './components/ProtectedRoutes';
+import NotAuthRoutes from './components/NotAuthRoutes';
 
 function Layout() {
    return (
       <div className='flex flex-col justify-between h-screen'>
-         <ToastContainer position='bottom-right' theme='light' pauseOnHover autoClose={2000} />
+         <ToastContainer position='top-center' theme='light' pauseOnHover autoClose={2000} />
          <Navbar />
          <main className='w-10/12 max-w-4xl mx-auto mb-auto'>
             <Outlet />
@@ -22,12 +28,28 @@ function Layout() {
 }
 
 function App() {
+   const dispatch = useDispatch();
+   const isAuth = useSelector((state) => state.user?.isAuth);
+   const { pathname } = useLocation();
+   console.log('isAuth : ', isAuth);
+
+   useEffect(() => {
+      if (isAuth) {
+         dispatch(authUser());
+      }
+   }, [isAuth, pathname, dispatch]);
+
    return (
       <Routes>
          <Route path='/' element={<Layout />}>
             <Route index element={<LandingPage />} />
-            <Route path='login' element={<LoginPage />} />
-            <Route path='register' element={<RegisterPage />} />
+            <Route element={<ProtectedRoutes isAuth={isAuth} />}>
+               <Route path='/protected' element={<ProtectedPage />} />
+            </Route>
+            <Route element={<NotAuthRoutes isAuth={isAuth} />}>
+               <Route path='/login' element={<LoginPage />} />
+               <Route path='/register' element={<RegisterPage />} />
+            </Route>
          </Route>
       </Routes>
    );
