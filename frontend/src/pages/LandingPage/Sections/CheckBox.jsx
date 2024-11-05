928d0b7592037b33442abc7c0df323a5fff191c3
vedivero <vedivero@github.com>
2024-11-06 02:17:51 +0900

    체크박스 컴포넌트 & 라디오박스 컴포넌트

--- frontend/src/pages/LandingPage/Sections/CheckBox.jsx
+++ frontend/src/pages/LandingPage/Sections/CheckBox.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-const CheckBox = () => {
-   return <div>CheckBox</div>;
+const CheckBox = ({ continents, checkedContinents, onFilters }) => {
+   const handleToggle = (continentId) => {
+      const currentIndex = checkedContinents.indexOf(continentId);
+
+      const newChecked = [...checkedContinents];
+
+      if (currentIndex === -1) {
+         newChecked.push(continentId);
+      } else {
+         newChecked.splice(currentIndex, 1);
+      }
+      onFilters(newChecked);
+   };
+
+   return (
+      <div className='flex p-2 mb-3 bg-gray-100 rounded-md flex-wrap justify-around'>
+         {continents?.map((continent) => (
+            <label // label 태그로 변경하여 클릭 이벤트를 확장
+               key={continent._id}
+               className='flex items-center mb-2 cursor-pointer' // 커서 스타일 추가로 클릭 가능하다는 시각적 표시
+            >
+               <input
+                  type='checkbox'
+                  onChange={() => handleToggle(continent._id)}
+                  checked={checkedContinents.indexOf(continent._id) !== -1}
+                  className='mr-1'
+               />
+               {continent.name}
+            </label>
+         ))}
+      </div>
+   );
 };
 
 export default CheckBox;
