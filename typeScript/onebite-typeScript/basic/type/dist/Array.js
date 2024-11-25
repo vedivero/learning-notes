3fff4d220b6775fab4ea28f090b661ae67978f02
vedivero <vedivero@github.com>
2024-11-26 00:54:18 +0900

    typeScript basic/primitive Type

---
+++ typeScript/onebite-typeScript/basic/type/dist/Array.js
@@ -0,0 +1,24 @@
+// Number Array
+let numArr = [1, 2, 3];
+// String Array
+let strArr = ['hello', 'type', 'script'];
+// boolean Array
+let boolArr = [true, false];
+// Multiple Array
+let MultipleArr = [1, 'hello'];
+// Multi-Array
+let doubleArr = [
+    [1, 2, 3],
+    [4, 5],
+];
+// tuple
+// 길이와 타입이 고정됀 배열
+let tuple1 = [1, 2];
+//tup1 = [1, 2, 3];
+//tu1 = ['1', '2'];
+tuple1.push(1);
+tuple1.pop();
+tuple1.pop();
+tuple1.pop();
+let tuple2 = [1, '2', true];
+export {};
