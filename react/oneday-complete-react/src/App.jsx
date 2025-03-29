160aef6189163a3059ca8456d5b8c5ad47adcee8
vedivero <vedivero73@gmail.com>
2025-03-29 21:33:49 +0900

    useState-practice2

--- react/oneday-complete-react/src/App.jsx
+++ react/oneday-complete-react/src/App.jsx
@@ -1,15 +1,34 @@
 import { useState } from 'react';
 import './App.css';
 
+//서버로 부터 응답 받는 데이터로 초기값 지정
+function queryItemsFromServer() {
+   return ['홍길동', '임꺽정'];
+}
+
 function App() {
-   const [value, setValue] = useState(0);
-   const handleClick = () => {
-      setValue(value + 1);
+   const [items, setItems] = useState(queryItemsFromServer);
+   const [value, setValue] = useState('');
+
+   const handleInputChange = (e) => {
+      setValue(e.target.value);
    };
+
+   const handleAddClick = () => {
+      // setItems([...items, value]);
+
+      setItems((previusState) => {
+         return [...previusState, value];
+      });
+   };
+
    return (
       <div>
-         <span>증가값 : {value}</span>
-         <button onClick={handleClick}>증가</button>
+         <input value={value} onChange={handleInputChange} type='text' />
+         <button onClick={handleAddClick}>추가</button>
+         {items.map((name, index) => (
+            <p key={index}>{name}</p>
+         ))}
       </div>
    );
 }
