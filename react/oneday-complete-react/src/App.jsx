8e15c8bf350cd744ec1bd5cff0ae040b002e4fc5
vedivero <vedivero73@gmail.com>
2025-03-31 10:10:16 +0900

    useMemo - practice1

--- react/oneday-complete-react/src/App.jsx
+++ react/oneday-complete-react/src/App.jsx
@@ -1,25 +1,37 @@
-import { useEffect, useRef } from 'react';
+import { useMemo, useState } from 'react';
 import './App.css';
 
-function App() {
-   // useRef의 또 다른 사용처 -> DOM객체를 참조
+const calculate = (number) => {
+   console.time('계산');
+   let result = 0;
+   //result += Math.sin(number);
+   for (let i = 0; i < 1400000; i++) {
+      result += Math.sin(i + number);
+   }
 
-   const inputRef = useRef(); // inputRef의 current가 input의 DOM을 참조하게 됨
+   console.timeEnd('계산');
+   return result;
+};
 
-   const login = () => {
-      alert(`환영합니다 , ${inputRef.current.value}`);
+export default function App() {
+   const [inputValue, setInputValue] = useState(1);
+
+   // 연산이 오래 걸리는 동작을 useMemo에 저장함으로써 UI 렌더링 반응 속도 향상
+   const calValue = useMemo(() => {
+      calculate(inputValue);
+   }, [inputValue]); // calculate 함수에 영향을 주는 두 번째 인자의 의존성 배열
+
+   const [count, setCount] = useState(0);
+   const increaseCount = () => {
+      setCount(count + 1);
    };
-
-   useEffect(() => {
-      inputRef.current.focus(); // input 태그 DOM을 참조 중
-   }, []);
 
    return (
       <div>
-         <input ref={inputRef} type='text' placeholder='사용자 이름' />
-         <button onClick={login}>로그인</button>
+         <h3>계산기</h3>
+         <input type='number' value={inputValue} onChange={(e) => setInputValue(parseInt(e.target.value))} />
+         <div>결과 = {calValue}</div>
+         <button onClick={increaseCount}>증가 : {count}</button>
       </div>
    );
 }
-
-export default App;
