f38da5d64a17b2aca7dbe2cfa5ae07625221064f
vedivero <vedivero73@gmail.com>
2025-03-31 21:01:09 +0900

    useLayoutEffect Hook

--- react/oneday-complete-react/src/App.jsx
+++ react/oneday-complete-react/src/App.jsx
@@ -1,65 +1,17 @@
-import { useReducer, useState } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import './App.css';
 
 export default function App() {
-   // useState
-   const [count1, setCount1] = useState(0);
+   //  웹 페이지 렌더링 후, useEffect의 콜백 함수 실행
+   useEffect(() => {
+      console.log('useEffect 콜백함수 실행');
+   });
 
-   // useReducer
-   const reducer = (currentState, action) => {
-      if (action.type === 'inc') return currentState + action.step;
-      else if (action.type === 'dec') return currentState - action.step;
-      else return currentState;
-   };
+   // useLayoutEffect의 콜백 함수가 먼저 실행 후, 웹 페이지 렌더링
+   // App 컴포넌트와 useEffect 사용 컴포넌트의 시간 차 렌더링을 해결하기 위함
+   useLayoutEffect(() => {
+      console.log('useLayoutEffect 콜백함수 실행');
+   });
 
-   const [count2, dispatch] = useReducer(reducer, 0);
-
-   const [step, setStep] = useState(1);
-
-   return (
-      <>
-         <div>
-            <button
-               onClick={() => {
-                  setCount1(count1 + 1);
-               }}
-            >
-               useState + 1
-            </button>{' '}
-            <button
-               onClick={() => {
-                  setCount1(count1 - 1);
-               }}
-            >
-               useState - 1
-            </button>{' '}
-            <span>{count1}</span>
-         </div>
-
-         <input
-            value={step}
-            onChange={(e) => {
-               setStep(e.target.value);
-            }}
-         />
-
-         <div>
-            <button
-               onClick={() => {
-                  dispatch({ type: 'inc', step: Number(step) });
-               }}
-            >
-               useReducer +
-            </button>{' '}
-            <button
-               onClick={() => {
-                  dispatch({ type: 'dec', step: Number(step) });
-               }}
-            >
-               useReducer -
-            </button>{' '}
-            <span>{count2}</span>
-         </div>
-      </>
-   );
+   return <></>;
 }
