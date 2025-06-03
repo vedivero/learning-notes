385cd5b2b212458eeed91e87c9aaf882892fe0d6
vedivero <vedivero73@gmail.com>
2025-06-03 14:23:22 +0900

    virus

---
+++ java/dfs-basic/src/virus_00_03.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class virus_00_03 {
+
+    static boolean[][] graph;
+    static boolean[] visited;
+    static int N,M;
+    static int answer;
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        graph = new boolean[N + 1][N + 1];
+        visited = new boolean[N + 1];
+
+        int x,y;
+        for(int i = 0; i < M; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            graph[x][y] = true;
+            graph[y][x] = true;
+        }
+    }
+}
