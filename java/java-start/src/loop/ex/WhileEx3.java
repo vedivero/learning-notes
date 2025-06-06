3e7685a5dc8cf8c893dc8f2e19dd44da734bc67f
vedivero <vedivero73@gmail.com>
2025-06-06 17:47:42 +0900

    loop

---
+++ java/java-start/src/loop/ex/WhileEx3.java
@@ -0,0 +1,15 @@
+package loop.ex;
+
+public class WhileEx3 {
+    public static void main(String[] args) {
+        int max = 10;
+        int sum = 0;
+        int i = 1;
+
+        while (i <= max) {
+            sum += i;
+            i++;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
