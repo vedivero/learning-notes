b085ffdd5b2f01c2990ad3bc8f237c69c5c34efd
vedivero <vedivero73@gmail.com>
2025-06-14 13:59:24 +0900

    class

---
+++ java/java-basic/src/ref/InitMain.java
@@ -0,0 +1,9 @@
+package ref;
+
+public class InitMain {
+    public static void main(String[] args) {
+        InitData initData = new InitData();
+        System.out.println("value 1 : "+ initData.value1);
+        System.out.println("value 2 : "+ initData.value2);
+    }
+}
