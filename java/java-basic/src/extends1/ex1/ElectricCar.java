07771daf1e8f5161cf5fee1c71c11cf8b7e486c9
vedivero <vedivero73@gmail.com>
2025-06-21 19:34:20 +0900

    extends

---
+++ java/java-basic/src/extends1/ex1/ElectricCar.java
@@ -0,0 +1,10 @@
+package extends1.ex1;
+
+public class ElectricCar {
+    public void move() {
+        System.out.println("자동차가 이동합니다.");
+    }
+    public void charge() {
+        System.out.println("자동차를 충전합니다.");
+    }
+}
---
+++ java/java-basic/src/extends1/ex1/GasCar.java
@@ -0,0 +1,11 @@
+package extends1.ex1;
+
+public class GasCar {
+    public void move() {
+        System.out.println("자동차가 이동합니다.");
+    }
+
+    public void fillUp() {
+        System.out.println("연료를 주유합니다.");
+    }
+}
