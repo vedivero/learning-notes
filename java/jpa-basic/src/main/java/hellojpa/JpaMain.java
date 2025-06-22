e06c725f46deeb17d81804c23a6e748a561ec602
vedivero <vedivero73@gmail.com>
2025-06-22 14:16:22 +0900

    Persistence Context

--- java/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ java/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,18 @@ public class JpaMain {
         transaction.begin();
 
         try {
+            // 비영속 상태 (아직 JPA와 관련 없는 상태/DB에 입력되지 않음)
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("Hello-JPA");
 
-            //Member findMember = entityManager.find(Member.class, 1L);
-            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class).getResultList();
+            // 영속 상태(영속성 컨텍스트에 의해 관리)
+            entityManager.persist(member);
 
-            for (Member member : result) {
-                System.out.println("member.name : "+member.getName());
-            }
+            // 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
+            // entityManager.detach(member);
 
-
+            // 트랜잭션 커밋하는 시점에서 DB에 쿼리가 실행
             transaction.commit();
         } catch (Exception e) {
             transaction.rollback();
