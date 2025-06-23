9db7c064b44f1a9ec781622e6d58c88c7ffbe57f
vedivero <vedivero73@gmail.com>
2025-06-23 23:00:37 +0900

    polymorphism

---
+++ java/java-basic/src/poly/basic/Child.java
@@ -0,0 +1,7 @@
+package poly.basic;
+
+public class Child extends Parent {
+    public void childMetohd() {
+        System.out.println("Child.childMetohd");
+    }
+}
---
+++ java/java-basic/src/poly/basic/PolyMain.java
@@ -0,0 +1,28 @@
+package poly.basic;
+
+public class PolyMain {
+    public static void main(String[] args) {
+
+        // 부모 변수가 부모 인스턴스 참조
+        System.out.println("Parent -> Parent");
+        Parent parent = new Parent();
+        parent.parentMethod();
+
+        // 자식 변수가 자식 인스턴스 참조
+        System.out.println("Child -> Child");
+        Child child = new Child();
+        child.parentMethod();
+        child.childMetohd();
+
+        // 부모 변수가 자식 인스턴스 참조(다형적 참조)
+        System.out.println("Parent -> Child");
+        Parent poly = new Child();
+        poly.parentMethod();
+
+        // 자식은 부모 인스턴스를 참조 X
+        //Child child1 = new Parent();
+
+        // 자식의 기능은 호출할 수 없다. 컴파일 오류 발생
+        //poly.childMethod();
+    }
+}
