620f746e39a70379006726d4d336b4277b6f6386
vedivero <vedivero73@gmail.com>
2025-06-25 19:46:15 +0900

    polymorphism

---
+++ java/java-basic/src/poly/ex2/AnimalPolyMain1.java
@@ -0,0 +1,18 @@
+package poly.ex2;
+
+public class AnimalPolyMain1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(cow);
+    }
+
+    private static void soundAnimal(Animal animal) {
+        animal.sound();
+    }
+}
+ 
