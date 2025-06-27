3d655831af0943444a4ca1eed113b70d3da401a2
hs.lee <hs.lee@astkorea.net>
2025-06-27 10:54:31 +0900

    직원 조회(+자격증 키워드) 검색 로직 추각

--- src/main/java/com/ast/pms/repository/EmployeeSpecification.java
+++ src/main/java/com/ast/pms/repository/EmployeeSpecification.java
@@ -1,6 +1,10 @@
 package com.ast.pms.repository;
 
 import com.ast.pms.domain.Employee;
+
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+
 import org.springframework.data.jpa.domain.Specification;
 
 public class EmployeeSpecification {
@@ -8,12 +12,16 @@ public class EmployeeSpecification {
     public static Specification<Employee> containsKeywordInFields(String keyword) {
         return (root, query, cb) -> {
             String likePattern = "%" + keyword.toLowerCase() + "%";
+
+            Join<Object, Object> licensesJoin = root.join("licenses", JoinType.LEFT);
+
             return cb.or(
                     cb.like(cb.lower(root.get("name")), likePattern),
                     cb.like(cb.lower(root.get("team")), likePattern),
                     cb.like(cb.lower(root.get("position")), likePattern),
                     cb.like(cb.lower(root.get("level")), likePattern),
-                    cb.like(cb.lower(root.get("workType")), likePattern));
+                    cb.like(cb.lower(root.get("workType")), likePattern),
+                    cb.like(cb.lower(licensesJoin.get("name")), likePattern));
         };
     }
 }
