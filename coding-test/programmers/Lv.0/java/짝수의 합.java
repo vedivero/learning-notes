aa0d22275f71b2d8835e51b97ef60f1a3864e304
vedivero73 <vedivero73@gmail.com>
2025-07-08 16:55:33 +0900

    짝수의 합

---
+++ coding-test/programmers/Lv.0/java/짝수의 합.java
@@ -0,0 +1,16 @@
+//정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+//입출력       예
+//n	        result
+//10	    30
+//4	        6
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
