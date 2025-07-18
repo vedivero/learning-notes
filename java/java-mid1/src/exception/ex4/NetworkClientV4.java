b0c6bb3fe915ef3635e2a6e2ae7ec8b3985a11e2
vedivero <vedivero73@gmail.com>
2025-07-18 21:52:48 +0900

    exception

---
+++ java/java-mid1/src/exception/ex4/NetworkClientV4.java
@@ -0,0 +1,43 @@
+package exception.ex4;
+
+import exception.ex4.exception.ConnectExceptionV4;
+import exception.ex4.exception.SendExceptionV4;
+
+public class NetworkClientV4 {
+
+    private final String address;
+    public boolean connectError;    //boolean의 기본값 : false
+    public boolean sendError;
+
+    public NetworkClientV4(String address) {
+        this.address = address;
+    }
+
+    public void connect() {
+
+        if (connectError) {
+            throw new ConnectExceptionV4(address, address + " 서버 연결 실패");
+        }
+        System.out.println(address + "서버 연결 성공");
+    }
+
+    public void send(String data) {
+        if (sendError) {
+            throw new SendExceptionV4(data, address + " 메시지 데이터 전송 실패");
+        }
+        System.out.println(address + " 서버에 데이터 전송 : " + data);
+    }
+
+    public void disconnct() {
+        System.out.println(address + " 서버 연결 해제");
+    }
+
+    public void initError(String data) {
+        if (data.contains("error1")) {
+            connectError = true;
+        }
+        if (data.contains("error2")) {
+            sendError = true;
+        }
+    }
+}
