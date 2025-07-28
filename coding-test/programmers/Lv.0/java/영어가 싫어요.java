02af20c193a9563913211df398467554db1ed005
vedivero73 <vedivero73@gmail.com>
2025-07-28 11:55:33 +0900

    대문자와 소문자

---
+++ coding-test/programmers/Lv.0/java/영어가 싫어요.java
@@ -0,0 +1,27 @@
+// 문제 설명
+// 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
+// 문자열 numbers가 매개변수로 주어질 때,
+// numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+
+// 입출력 예
+// numbers	result
+// "onetwothreefourfivesixseveneightnine"	123456789
+// "onefourzerosixseven"	14067
+
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+
+        String[] words = new String[]{
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
+        };
+
+        for (int i = 0; i < ; i++) {
+
+        }
+
+
+        return answer;
+    }
+}
