1738d5f3b2dc7256f38002db371c9898748fccee
vedivero <vedivero73@gmail.com>
2025-07-30 23:15:28 +0900

    link

---
+++ java/java-mid2/src/collection/link/NodeMain3.java
@@ -0,0 +1,66 @@
+package collection.link;
+
+public class NodeMain3 {
+
+    public static void main(String[] args) {
+
+        // 노드 생성하고 연결하기 : A -> B -> C
+        Node first = new Node("A");
+        first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        System.out.println("연결된 노드 출력하기");
+        System.out.println("first = " + first);
+        
+        //모든 노드 탐색하기
+        System.out.println("모든 노드 탐색하기");
+        printAll(first);
+
+        //마지막 노드 조회
+        Node lastNode = getLastNode(first);
+
+        //특정 index의 노드 조회
+        int index = 2;
+        Node index2Node = getNode(first, index);
+        System.out.println("index2Node.item = " + index2Node.item);
+
+        //데이터 추가하기
+        System.out.println("데이터 추가하기");
+        add(first, "D");
+        System.out.println("first = " + first);
+        add(first, "E");
+        System.out.println("first = " + first);
+        add(first, "F");
+        System.out.println("first = " + first);
+    }
+
+    private static void printAll(Node node) {
+        Node x = node;
+        while (x != null) {
+            System.out.println(x.item);
+            x = x.next;
+        }
+    }
+
+    private static Node getLastNode(Node node) {
+        Node x = node;
+        while (x.next != null) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    private static Node getNode(Node node, int index) {
+        Node x = node;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    private static void add(Node node, String param) {
+        Node lastNode = getLastNode(node);
+        lastNode.next = new Node(param);
+    }
+
+}
