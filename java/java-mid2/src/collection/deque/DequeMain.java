87e647d87b2c7bcb2819a5a0e4861b7c69955b93
vedivero <vedivero73@gmail.com>
2025-08-13 00:04:47 +0900

    stack, queue, deque

---
+++ java/java-mid2/src/collection/deque/DequeMain.java
@@ -0,0 +1,29 @@
+package collection.deque;
+
+import java.util.ArrayDeque;
+
+public class DequeMain {
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        deque.offerFirst(1);
+        System.out.println("deque = " + deque);
+        deque.offerFirst(2);
+        System.out.println("deque = " + deque);
+        deque.offerFirst(3);
+        System.out.println("deque = " + deque);
+
+        deque.offerLast(4);
+        System.out.println("deque = " + deque);
+
+        System.out.println("deque.peekFirst() = " + deque.peekFirst());
+        System.out.println("deque.peekLast() = " + deque.peekLast());
+
+        System.out.println("deque.pollFirst() = " + deque.pollFirst());
+        System.out.println("deque.pollLast() = " + deque.pollLast());
+
+        System.out.println("deque = " + deque);
+
+    }
+}
