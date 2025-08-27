d973761c6cd2120226ad0f41b0a60cb63541f9b1
vedivero73 <vedivero73@gmail.com>
2025-08-27 14:15:56 +0900

    list

---
+++ java/review/review1/src/collection/list/test/ex2/ShoppingCartMain.java
@@ -0,0 +1,17 @@
+package collection.list.test.ex2;
+
+public class ShoppingCartMain {
+
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+
+        Item item1 = new Item("삼겹살", 12_000, 3);
+        Item item2 = new Item("라면", 3_000, 10);
+
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        cart.displayItems();
+    }
+    
+}
