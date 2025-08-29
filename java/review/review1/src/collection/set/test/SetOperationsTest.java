fe0b26e9555d5136bb176e4e91e62a707ac37f69
vedivero73 <vedivero73@gmail.com>
2025-08-29 12:39:15 +0900

    Set

---
+++ java/review/review1/src/collection/set/test/SetOperationsTest.java
@@ -0,0 +1,31 @@
+package collection.set.test;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class SetOperationsTest {
+
+    public static void main(String[] args) {
+
+        HashSet<Integer> hashSet1 = new HashSet<>(List.of(1, 2, 3, 4, 5));
+        HashSet<Integer> hashSet2 = new HashSet<>(List.of(3, 4, 5, 6, 7));
+
+        HashSet<Integer> union = new HashSet<>(hashSet1);
+        union.addAll(hashSet2);
+        System.out.println(" ");
+        System.out.println("합집합 - 전체 수");
+        System.out.println("union = " + union);
+
+        HashSet<Integer> intersection = new HashSet<>(hashSet1);
+        intersection.retainAll(hashSet2);
+        System.out.println(" ");
+        System.out.println("교집합 - 공통된 수");
+        System.out.println("intersection = " + intersection);
+
+        HashSet<Integer> difference = new HashSet<>(hashSet1);
+        difference.removeAll(hashSet2);
+        System.out.println(" ");
+        System.out.println("차집합 - 나머지 수");
+        System.out.println("difference = " + difference);
+    }
+}
---
+++ java/review/review1/src/collection/set/test/UniqueNamesTest2.java
@@ -0,0 +1,22 @@
+package collection.set.test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class UniqueNamesTest2 {
+
+    public static void main(String[] args) {
+
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>(Arrays.asList(inputArr));
+
+        /*Iterator<Integer> iterator = linkedHashSet.iterator();
+
+        while (iterator.hasNext()) {
+            linkedHashSet.add(iterator.next());
+        }*/
+
+    }
+}
