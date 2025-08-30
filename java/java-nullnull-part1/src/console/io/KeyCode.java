bdfcd922aad6f55967f6adb131da992538eba0d0
vedivero <vedivero73@gmail.com>
2025-08-30 14:07:07 +0900

    varaiable, comment

---
+++ java/java-nullnull-part1/src/console/io/KeyCode.java
@@ -0,0 +1,20 @@
+package console.io;
+
+import java.io.IOException;
+
+public class KeyCode {
+    public static void main(String[] args) throws IOException {
+
+        int keyCode = System.in.read();
+        System.out.println("keyCode = " + keyCode);
+        keyCode = System.in.read();
+        System.out.println("keyCode = " + keyCode);
+        keyCode = System.in.read();
+        System.out.println("keyCode = " + keyCode);
+        keyCode = System.in.read();
+        System.out.println("keyCode = " + keyCode);
+        keyCode = System.in.read();
+        System.out.println("keyCode = " + keyCode);
+
+    }
+}
