58712e78818b8c6fd93eb8a43b1cea6b002833ee
vedivero73 <vedivero73@gmail.com>
2025-09-03 10:39:49 +0900

    Collection

---
+++ java/review/review1/src/collection/compare/SortMain4.java
@@ -0,0 +1,35 @@
+package collection.compare;
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class SortMain4 {
+
+    public static void main(String[] args) {
+
+        MyUser myUser1 = new MyUser("a", 30);
+        MyUser myUser2 = new MyUser("b", 20);
+        MyUser myUser3 = new MyUser("c", 10);
+
+        List<MyUser> list = new LinkedList<>();
+
+        list.add(myUser1);
+        list.add(myUser2);
+        list.add(myUser3);
+
+        System.out.println(" ");
+        System.out.println("기본 데이터");
+        System.out.println("list = " + list);
+
+        System.out.println(" ");
+        System.out.println("Comparable 기본 정렬");
+        list.sort(null);
+        System.out.println("list = " + list);
+
+        System.out.println(" ");
+        System.out.println("IdComparator 정렬");
+        list.sort(new IdComparator());
+        System.out.println("list = " + list);
+
+    }
+}
---
+++ java/review/review1/src/collection/utils/EmptyListMain.java
@@ -0,0 +1,18 @@
+package collection.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EmptyListMain {
+
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list3 = Collections.emptyList();
+        List<Integer> list4 = List.of();
+
+        System.out.println("list3 = " + list3.getClass());
+        System.out.println("list4 = " + list4.getClass());
+    }
+}
