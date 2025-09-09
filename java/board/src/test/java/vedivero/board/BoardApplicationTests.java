1ac5f0ab71ebcc54f200475c869b6c65c1dbe0c5
vedivero <vedivero73@gmail.com>
2025-09-09 23:47:14 +0900

    Board Project Setting

---
+++ java/board/src/test/java/vedivero/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package vedivero.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
