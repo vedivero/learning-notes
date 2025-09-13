9e0c6aca5f2fcc0ecdef52faa9e37788b0583004
vedivero <vedivero73@gmail.com>
2025-09-13 22:41:19 +0900

    Data Initialize - multi thread with CountDownLatch

---
+++ java/board/service/article/src/test/java/vedivero/board/article/data/DataInitializer.java
@@ -0,0 +1,60 @@
+package vedivero.board.article.data;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import kuke.board.common.snowflake.Snowflake;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.support.TransactionTemplate;
+import vedivero.board.article.entity.Article;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class DataInitializer {
+    @PersistenceContext
+    EntityManager entityManager;
+    @Autowired
+    TransactionTemplate transactionTemplate;
+
+    //게시글 삽입 목적
+    Snowflake snowflake = new Snowflake();
+    CountDownLatch latch = new CountDownLatch(EXCUTE_COUNT);
+
+    static final int BULK_INSERT_SIZE = 2_000;
+    static final int EXCUTE_COUNT = 6_000;
+
+    //멀티쓰레드로 수행
+    @Test
+    void initialize() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < EXCUTE_COUNT; i++) {
+            executorService.submit(() -> {
+                insert();
+                latch.countDown();
+                System.out.println("latch.getCount() = " + latch.getCount());
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+    }
+
+    //트랜잭션 템플릿으로 데이터 삽입
+    void insert() {
+        transactionTemplate.executeWithoutResult(status->{
+            for (int i = 0; i < BULK_INSERT_SIZE; i++) {
+                Article article = Article.create(
+                        snowflake.nextId(),
+                        "title" + i,
+                        "content" + i,
+                        1L,
+                        1L
+                );
+                entityManager.persist(article);
+            }
+        });
+    }
+}
