5348a07059a5a2a2ba613b40536e601758ef3656
vedivero <vedivero73@gmail.com>
2025-11-06 21:04:54 +0900

    thread practice

--- java/java-adv1/src/thread/start/test/StartTest5Main.java
+++ java/java-adv1/src/thread/start/test/StartTest5Main.java
@@ -3,39 +3,33 @@ package thread.start.test;
 import static util.MyLogger.log;
 
 public class StartTest5Main {
-
-    public static void main(String[] args) {
-
-        Thread threadA = new Thread(new PrintWork("A",1000));
+    Thread threadA = new Thread(new PrintWork("A",1000));
         threadA.start();
 
-        Thread threadB = new Thread(new PrintWork("B", 500));
+    Thread threadB = new Thread(new PrintWork("B", 500));
         threadB.start();
 
+}
+
+static class PrintWork implements Runnable {
+
+    private String content;
+    private int sleepMs;
+
+    public PrintWork(String content, int sleepMs) {
+        this.content = content;
+        this.sleepMs = sleepMs;
     }
 
-    static class PrintWork implements Runnable {
-
-        private String content;
-        private int sleepMs;
-
-        public PrintWork(String content, int sleepMs) {
-            this.content = content;
-            this.sleepMs = sleepMs;
-        }
-
-        @Override
-        public void run() {
-
-            while (true) {
-                try {
-                    Thread.sleep(sleepMs);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                log(content);
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-
+            log("B");
         }
     }
 }
