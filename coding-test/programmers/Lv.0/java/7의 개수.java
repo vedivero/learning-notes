aa16b9d26e5bfb78ab513b6710261e7f25a6c226
vedivero73 <vedivero73@gmail.com>
2025-11-25 11:51:02 +0900

    7의 개수.java

--- coding-test/programmers/Lv.0/java/7의 개수.java
+++ coding-test/programmers/Lv.0/java/7의 개수.java
@@ -2,8 +2,10 @@ class Solution {
     public int solution(int[] array) {
         int answer = 0;
 
+        // 매개 변수로 전달된 숫자형 배열 array를 향상된 반복문으로 순회
         for (int num : array) {
 
+            // 숫자형 데이터 num을 문자열로 형 변환
             String s = String.valueOf(num);
 
             for (char c : s.toCharArray()) {
